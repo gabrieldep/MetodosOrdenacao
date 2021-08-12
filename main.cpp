36783c2bb095cfa7cee89636f3529a3a136666ed
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-08-12 10:38:04 -0300

    Criado metodo para preencher vetor a partir do arquivo lido;

--- main.cpp
+++ main.cpp
@@ -2,12 +2,11 @@
 #include "Informacoes.h"
 #include "Quicksort.h"
 using namespace std;
-int main(int argc, char* argv[])
-{
-	int entrada = 200000;
-	Informacoes* informacoes = new Informacoes[entrada]();
-	char Linha[100];
+
+void PreencheVetor(Informacoes* informacoes[], char* caminho[]) {
 	char* result;
+	char Linha[100];
+	//*caminho = "C:/Users/Gabriel/Documents/homologacao.txt";
 	FILE* arquivo = fopen("C:/Users/Gabriel/Documents/homologacao.txt", "rt");
 	int aux = 0;
 	while (!feof(arquivo))
@@ -15,28 +14,30 @@ int main(int argc, char* argv[])
 		result = fgets(Linha, 100, arquivo);
 		if (result == NULL)break;
 		string s = result;
-		string nome;
-		string dados;
 		for (int i = 0; i < s.size(); i++) {
 			if (s[i] == ' ') {
-				nome = s.substr(0, i);
-				dados = s.substr(i + 1, s.size());
+				(*informacoes)[aux].SetPrimeiraString(s.substr(0, i));
+				(*informacoes)[aux].SetSegundaString(s.substr(i + 1, s.size()));
 				break;
 			}
 		}
-
-		informacoes[aux].SetPrimeiraString(nome);
-		informacoes[aux].SetSegundaString(dados);
 		aux++;
 	}
 	fclose(arquivo);
+}
 
+int main(int argc, char* argv[])
+{
+	int entrada = 200000;
+	Informacoes* informacoes = new Informacoes[entrada]();
 
-	Quicksort(false).OrdenaQuicksort(informacoes, entrada);
+	PreencheVetor(&informacoes, &argv[1]);
+
+	Quicksort(true).OrdenaQuicksort(informacoes, entrada);
 
 
 	for (int i = 0; i < entrada; i++) {
-		cout << informacoes[i].GetSegundaString() << endl;
+		cout << informacoes[i].GetPrimeiraString() << endl;
 	}
 	return 0;
 }
