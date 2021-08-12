08459ca7dc0e2ba6a3243269b640b5d77b1b14f5
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-08-11 22:07:20 -0300

    add: Metodo para comparar strings

---
+++ OrdenacaoControl.cpp
@@ -0,0 +1,14 @@
+#include "OrdenacaoControl.h"
+
+bool OrdenacaoControl::VemAntes(std::string primeira, std::string segunda)
+{
+	int tamanho = primeira.size() > segunda.size() ?
+		segunda.size() : primeira.size();
+
+	for (int i = 0; i < tamanho; i++) {
+		if (primeira[i] != segunda[i]) {
+			return primeira[i] < segunda[i];
+		}
+	}
+	return primeira.size() < segunda.size();
+}
---
+++ OrdenacaoControl.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+class OrdenacaoControl
+{
+	public:
+		bool VemAntes(std::string primeira, std::string segunda);
+};
+
