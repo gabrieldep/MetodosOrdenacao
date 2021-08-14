69b2fe648bbe0c5a541d1a6b8d8a60d30a08c303
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-08-14 15:54:32 -0300

    RadixSort ordenando pela primeira casa de bit;

--- Radixsort.cpp
+++ Radixsort.cpp
@@ -1,6 +1,5 @@
 #include "Radixsort.h"
 #include "OrdenacaoControl.h"
-
 Radixsort::Radixsort()
 {
 }
@@ -11,8 +10,33 @@ Radixsort::~Radixsort()
 
 void Radixsort::Ordena(Informacoes informacoes[], int n)
 {
+	OrdenaRadixsort(0, n - 1, 0, informacoes);
 }
 
-void Radixsort::OrdenaRadixsort(int esq, int dir, int w, Informacoes informacoes[])
+void Radixsort::OrdenaRadixsort(int esq, int dir, int indexBit, Informacoes informacoes[])
 {
+	int const maxBit = 7;
+
+	Informacoes aux;
+	int i = esq, j = dir, w = indexBit;
+	if (indexBit > maxBit) {
+		return;
+	}
+
+	do {
+		while (informacoes[i].GetDados()[indexBit] == '0') {
+			i++;
+		}
+		while (informacoes[j].GetDados()[indexBit] == '1') {
+			j++;
+		}
+
+		if (i <= j) {
+			aux = informacoes[i];
+			informacoes[i] = informacoes[j];
+			informacoes[j] = aux;
+			(i)++;
+			(j)--;
+		}
+	} while (i <= j);
 }
